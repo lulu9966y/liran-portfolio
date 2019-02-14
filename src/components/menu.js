11938a60c52ffbe9ef144186e75842fce320aa90
Jasmine <ly4938@rit.edu>
2019-02-14 00:29:40 -0500

    background effect 02142019

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,20 +1,72 @@
 import React from 'react'
 import './menu.css'
-import {Link} from 'gatsby'
+import { Link } from 'gatsby'
 
 class Menu extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      changed: false,
+      index: 0,
+    }
+    this.changeBackground1 = this.changeBackground1.bind(this)
+    this.changeBackground2 = this.changeBackground2.bind(this)
+    this.changeBackground3 = this.changeBackground3.bind(this)
+    this.changeBackground4 = this.changeBackground4.bind(this)
+    this.changeBackground5 = this.changeBackground5.bind(this)
+    this.changeBackground6 = this.changeBackground6.bind(this)
+  }
+
+  changeBackground1() {
+    this.setState({ changed: !this.state.changed, index: 1 })
+  }
+  changeBackground2() {
+    this.setState({ changed: !this.state.changed, index: 2 })
+  }
+  changeBackground3() {
+    this.setState({ changed: !this.state.changed, index: 3 })
+  }
+  changeBackground4() {
+    this.setState({ changed: !this.state.changed, index: 4 })
+  }
+  changeBackground5() {
+    this.setState({ changed: !this.state.changed, index: 5 })
+  }
+  changeBackground6() {
+    this.setState({ changed: !this.state.changed, index: 6 })
   }
 
   render() {
+    let button = {
+    };
+    if(this.state.changed) {
+      button.background = 'transparent';
+    }else {
+      button.transition = '1s';
+    }
     return (
       <div className="Menu">
-      <div className = "MenuBackground" />
+        <div
+          className={
+            this.state.changed && this.state.index == '1'
+              ? 'MenuBackground MenuBackgroundChanged1'
+              : this.state.changed && this.state.index == '3'
+              ? 'MenuBackground MenuBackgroundChanged3'
+              : this.state.changed && this.state.index == '5'
+              ? 'MenuBackground MenuBackgroundChanged5'
+              : this.state.changed == false && this.state.index == '2'
+              ? 'MenuBackground MenuBackgroundChanged2'
+              : this.state.changed == false && this.state.index == '4'
+              ? 'MenuBackground MenuBackgroundChanged4'
+              : this.state.changed == false && this.state.index == '6'
+              ? 'MenuBackground MenuBackgroundChanged6'
+              :'MenuBackground'
+          }
+        />
         <div className="MenuLine" />
         <div className="Header">
-          <button onClick={this.props.closeMenu}>
-            <div className="Wrap" />
+          <button onClick={this.props.closeMenu} style = {button}>
+            <div className="Wrap" style = {button} />
             <div className="Two">
               <div className="Left" />
               <div className="Right" />
@@ -22,15 +74,44 @@ class Menu extends React.Component {
           </button>
         </div>
         <div className="MenuBoard">
-          <Link to="/646propjects/" className="MenuItem">Web Development</Link>
-          <Link to="/646propjects/" className="MenuItem">UX Case Studies</Link>
-          <Link to="/646propjects/" className="MenuItem">About Me</Link>
+          <Link
+            to="/646propjects/"
+            className="MenuItem"
+            onMouseOver={this.changeBackground5}
+            onMouseOut={this.changeBackground6}
+          >
+            Web Development
+          </Link>
+          <Link
+            to="/646propjects/"
+            className="MenuItem"
+            onMouseOver={this.changeBackground3}
+            onMouseOut={this.changeBackground4}
+          >
+            UX Case Studies
+          </Link>
+          <Link
+            to="/646propjects/"
+            className="MenuItem"
+            onMouseOver={this.changeBackground1}
+            onMouseOut={this.changeBackground2}
+          >
+            About Me
+          </Link>
         </div>
         <div className="SocialBoard">
-          <a href="https://www.instagram.com/lulunam/" className="SocialItem">INSTAGRAM</a>
-          <a href="https://github.com/lulu9966y" className="SocialItem">GITHUB</a>
-          <a href="https://github.com/lulu9966y" className="SocialItem">LINKEDIN</a>
-          <a href="mailto:liran.yin@gmail.com" className="SocialItem">LIRAN.YIN@GMAIL.COM</a>
+          <a href="https://www.instagram.com/lulunam/" className="SocialItem">
+            INSTAGRAM
+          </a>
+          <a href="https://github.com/lulu9966y" className="SocialItem">
+            GITHUB
+          </a>
+          <a href="https://github.com/lulu9966y" className="SocialItem">
+            LINKEDIN
+          </a>
+          <a href="mailto:liran.yin@gmail.com" className="SocialItem">
+            LIRAN.YIN@GMAIL.COM
+          </a>
         </div>
       </div>
     )
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,18 +5,31 @@ import Layout from '../components/layout'
 import Header from '../components/header'
 import Footer from '../components/footer'
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`liran`, `yin`, `portfolio`]} />
-    <Header />
-    <div className="Dashboard">
-      <h1>Hi. I’m Liran. I’m a web developer. </h1>
-      <p>I’m currently based in Rochester enrolling in Human-Computer Interaction program at RIT. I’m passinate about designing and coding web applications with focus on not just <Link to="/646propjects/">what it looks like</Link>, but <Link to="/page-2/">how it works</Link>. If you want to know more <Link to="/page-2/">about me</Link>, feel free to contact me on liran.yin@gmail.com.
-      </p>
-    </div>
-    <Footer />
-  </Layout>
-)
+class IndexPage extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <Layout>
+        <SEO title="Home" keywords={[`liran`, `yin`, `portfolio`]} />
+        <Header />
+        <div className="Dashboard">
+          <h1>Hi. I’m Liran. I’m a web developer. </h1>
+          <p>
+            I’m currently based in Rochester enrolling in Human-Computer
+            Interaction program at RIT. I’m passinate about designing and coding
+            web applications with focus on not just{' '}
+            <Link to="/646propjects/">what it looks like</Link>, but{' '}
+            <Link to="/page-2/">how it works</Link>. If you want to know more{' '}
+            <Link to="/page-2/">about me</Link>, feel free to contact me on{' '}
+            <span>liran.yin@gmail.com</span>.
+          </p>
+        </div>
+        <Footer />
+      </Layout>
+    )
+  }
+}
 
 export default IndexPage
-
